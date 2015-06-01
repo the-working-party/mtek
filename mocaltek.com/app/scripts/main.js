a87e9458dcfb9cd3802c17850ee4c256dc1b790f
Geoff Irvine <mail@geoffirvine.com>
2015-06-01 15:44:02 +1000

    Change mocaltek microsite to incorporate chainformation page

---
+++ mocaltek.com/app/scripts/main.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+    
+    /*$('.feature').matchHeight();*/
+
+
+
+    // Menu overlay
+ 
+
+    var $overlay_container = $(".overlay-container");
+
+    $(".js-overlay-button").click(function(){
+        var overlay_id;
+        
+        overlay_id=$(this).data("overlay-target");
+        
+        $overlay_container.addClass("active");
+        $overlay_container.find('.overlay').removeClass('active');
+        $overlay_container.find('[data-overlay="' + overlay_id + '"]').addClass('active');
+        
+        $('body').addClass('overlay-open');
+
+        
+        
+    });
+
+    $(".js-overlay-close").click(function(){
+        $overlay_container.removeClass('active');
+        $overlay_container.find('.overlay').removeClass('active');
+        
+        $('body').removeClass('overlay-open');
+        
+    });
+
+
+    
+    
+    var wow = new WOW({
+    boxClass: 'wow', // animated element css class (default is wow)
+    animateClass: 'animated', // animation css class (default is animated)
+    offset: 0, // distance to the element when triggering the animation (default is 0)
+    mobile: false, // trigger animations on mobile devices (default is true)
+    live: true, // act on asynchronously loaded content (default is true)
+    callback: function (box) {
+        // the callback is fired every time an animation is started
+        // the argument that is passed in is the DOM node being animated
+    }
+});
+wow.init();
+    
+    
+}); // END ONREADY  
+
